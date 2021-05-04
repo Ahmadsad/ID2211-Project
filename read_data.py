18fcb5b3626a997ca3419482bc39d15fcd1e041f
ahmad <54584161+ahmadsad@users.noreply.github.com>
2021-05-04 11:44:41 +0200

    did some more experimenting and read image in liver regions

--- read_data.py
+++ read_data.py
@@ -13,7 +13,7 @@ print("The type of 'image obj' is {}.".format(type(im_obj)));
 
 # Too see which information you might retrieve from an 'Image' object, you can use the command 'dir'.
 # It will print a lot of functions. However, you should focus on those on the top (without the trailing '__').
-dir(sitk.Image);
+# dir(sitk.Image);
 
 print("\nSize: {}".format(im_obj.GetSize()))
 print("Spacing: {}".format(im_obj.GetSpacing()))
@@ -22,22 +22,24 @@ print("Depth: {}".format(im_obj.GetDepth()))
 print("Height: {}".format(im_obj.GetHeight()))
 print("Width: {}".format(im_obj.GetWidth()))
 print("NumberOfComponentsPerPixel: {}".format(im_obj.GetNumberOfComponentsPerPixel()))
-print(im_obj)
+# print(im_obj)
 
 # Use the GetArrayViewFromImage function to plot the loaded image with pyplot's imshow
 im_array = sitk.GetArrayViewFromImage(im_obj)
-print("im_array has typ= {} \n and has dimension= {} \n and shape= {}".format(type(im_array), im_array.ndim, im_array.shape));
+print("Obtained image_array from DICOM file has typ= {} \n and has dimension= {} \n and shape= {}".format(type(im_array), im_array.ndim, im_array.shape));
 
 # squeez extra dimensions
 im_array_squeezed = im_array.squeeze()
-print("\nafter squeezing, im_array has shape= {}".format(im_array_squeezed.shape))
+print("\nAfter squeezing the dimension of image_array, image_array has shape= {}".format(im_array_squeezed.shape))
 
 # plot the image
 # plt.figure()
 # plt.imshow(im_array_squeezed, cmap='gray')
 # plt.show();
 print(im_array_squeezed)
-print(np.unique(im_array_squeezed))
+print('Total number of values in the image array: ',len(np.unique(im_array_squeezed)));
+print('The maximum value is: {}, and the minimum value is: {}'.format(np.max(im_array_squeezed), np.min(im_array_squeezed)))
+print('The mean value is: ', np.mean(im_array_squeezed));
 # optional normalization
 im_arrayNor = im_array_squeezed-np.mean(im_array_squeezed);
 im_arrayNor = im_arrayNor/np.max(im_arrayNor);
@@ -45,9 +47,10 @@ im_arrayNor = im_arrayNor/np.max(im_arrayNor);
 # plt.figure()
 # plt.imshow(im_arrayNor, cmap='gray')
 # plt.show();
-print('\n', np.unique(im_arrayNor))
+print('After normalizing (divide by max value) the array have values:\n', np.unique(im_arrayNor));
+print('The maximum value is: {}, and the minimum value is: {}'.format(np.max(im_arrayNor), np.min(im_arrayNor)))
 
 # eventuelly for saving as png
 # imwrite(str("testImagio_dicom.png"), im_arrayNor/np.max(im_arrayNor))
-test = imread("testImagio_dicom.png");
-print('\n',np.unique(test))
+# test = imread("testImagio_dicom.png");
+# print('\n',np.unique(test))
